386e638347ad5a070ede0a4a9ec5d9acb5b98b81
kieran Sweetman <kikisweetman@gmail.com>
2022-12-06 17:32:51 +0100

    added markers + information

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // villes par population en ordre
 
+
+
 const cities = ['Paris', 'Marseille', 'Lyon', 'Toulouse', 'Nice', 'Nantes', 'Montpellier', 'Strasbourg', 'Bordeaux', 'Lille' ]
 
 let buildUrl = (city) =>{
@@ -29,14 +31,64 @@ async function request(city) {
     return data;    
 };
 
-const city = await request().catch(error => {
-    error.message;
-});
 
 async function printCities(){
 
     cities.forEach(async city=>{
         let req = await request(city);
-        console.log(req);
+        let data = await req.data;
+        console.log(data);
+
+        let circle = L.circle([data.city.geo[0], data.city.geo[1],] ,{color: airQualityMarker(data.aqi), radius:20000})
+        .bindPopup(`o3 :${data.iaqi.o3.v}<br>
+                    pm25 :${data.iaqi.pm25.v}<br> 
+                    pm10 :${data.iaqi.pm10.v}<br> 
+                    NO2 :${data.iaqi.no2.v}<br> 
+                    SO2 :${data.iaqi}v<br> 
+                    co :${data.iaqi}<br> `)
+        .openPopup().addTo(map);
+        // pm25 pm10-> data.forecast.daily.pm10 03 no2 so2 co
+        
     })
 }
+
+
+async function airQuality(){
+     cities.forEach(async city => {
+        let req = await request(city);
+        let data = await req.data;
+     })
+}
+
+function airQualityMarker(aqi){
+
+    let color = ""
+
+    if(aqi < 25){
+     color = 'blue';
+     return color;
+    }
+    if(aqi < 50){
+     color = 'green';
+     return color;
+    }
+    if(aqi < 75){
+     color = 'yellow';
+     return color;
+    }
+    if(aqi < 100){
+     color = 'orange';
+     return color;
+    }
+    if(aqi < 125){
+     color = 'red';
+     return color;
+    }
+    
+
+}
+
+
+
+await printCities();
+
